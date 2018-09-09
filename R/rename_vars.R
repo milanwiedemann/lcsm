18d62e7f04b138420d4705a53d1a81c6f5a99d47
Milan Wiedemann <milan.wiedemann@gmail.com>
2018-09-09 14:11:47 +0200

    use consistent argument names across functions

--- R/rename_vars.R
+++ R/rename_vars.R
@@ -1,42 +1,42 @@
 #' Rename variables for univariate and bivariate models
 #'
 #' @param data Dataframe in wide format
-#' @param x_vars List of variables measuring first construct
-#' @param y_vars List of variables measuring second construct
+#' @param var_x List of variables measuring first construct
+#' @param var_y List of variables measuring second construct
 #'
 #' @return Dataframe in wide format with renamed variables
 #' @export
 #'
 #' @examples
-rename_vars <- function(data, x_vars, y_vars){
+rename_lcsm_vars <- function(data, var_x, var_y){
 
-  # Rename variables in x_vars and y_vars list
-  if (base::exists("x_vars") == TRUE & base::exists("y_vars") == TRUE) {
+  # Rename variables in var_x and var_y list
+  if (base::exists("var_x") == TRUE & base::exists("var_y") == TRUE) {
     
-    for (i in base::seq_along(y_vars)) {
-      base::names(data)[base::names(data) == x_vars[i]] <- base::paste("x", i, sep = "")
-      base::names(data)[base::names(data) == y_vars[i]] <- base::paste("y", i, sep = "")
+    for (i in base::seq_along(var_y)) {
+      base::names(data)[base::names(data) == var_x[i]] <- base::paste("x", i, sep = "")
+      base::names(data)[base::names(data) == var_y[i]] <- base::paste("y", i, sep = "")
     }
     # Return dataframe with renamed variables
     data
   }
   
-  # Rename variables in x_vars list
-  if (base::exists("x_vars") == TRUE) {
+  # Rename variables in var_x list
+  if (base::exists("var_x") == TRUE) {
     
-    for (i in base::seq_along(x_vars)) {
-      base::names(data)[base::names(data) == x_vars[i]] <- base::paste("x", i, sep = "")
+    for (i in base::seq_along(var_x)) {
+      base::names(data)[base::names(data) == var_x[i]] <- base::paste("x", i, sep = "")
     }
     
     # Return dataframe with renamed variables
     data
   }
   
-  # Rename variables in y_vars list
-  if (base::exists("y_vars") == TRUE) {
+  # Rename variables in var_y list
+  if (base::exists("var_y") == TRUE) {
     
-    for (i in base::seq_along(y_vars)) {
-      base::names(data)[base::names(data) == y_vars[i]] <- base::paste("y", i, sep = "")
+    for (i in base::seq_along(var_y)) {
+      base::names(data)[base::names(data) == var_y[i]] <- base::paste("y", i, sep = "")
     }
     
     # Return dataframe with renamed variables
