72e74a9aace2ed44c9393bd81c05ed0752692b2d
Milan Wiedemann <milan.wiedemann@gmail.com>
2018-09-21 14:40:55 +0100

    if not specified est model parameters in list to FALSE

--- R/specify_lavaan_bi_model.R
+++ R/specify_lavaan_bi_model.R
@@ -38,6 +38,39 @@ specify_lavaan_bi_model <- function(timepoints,
                                     change_letter_y = "j"
                                     ) {
   
+  # Code parameters in model_x that are not defined as FALSE
+  if (is.null(model_x$delta_xy) == TRUE) {
+    model_x$delta_xy <- FALSE
+  }
+  
+  if (is.null(model_x$delta_yx) == TRUE) {
+    model_x$delta_yx <- FALSE
+  }
+  
+  if (is.null(model_x$xi_xy) == TRUE) {
+    model_x$xi_xy <- FALSE
+  }
+  
+  if (is.null(model_x$xi_yx) == TRUE) {
+    model_x$xi_yx <- FALSE
+  }
+  
+  # Code parameters in model_y that are not defined as FALSE
+  if (is.null(model_y$delta_xy) == TRUE) {
+    model_y$delta_xy <- FALSE
+  }
+  
+  if (is.null(model_y$delta_yx) == TRUE) {
+    model_y$delta_yx <- FALSE
+  }
+  
+  if (is.null(model_y$xi_xy) == TRUE) {
+    model_y$xi_xy <- FALSE
+  }
+  
+  if (is.null(model_y$xi_yx) == TRUE) {
+    model_y$xi_yx <- FALSE
+  }
   
   model_x_uni_lavaan <- ""
   model_x_uni_lavaan <- lcsm::specify_lavaan_uni_model(timepoints = timepoints, model = model_x, variable = var_x, change_letter = change_letter_x)
@@ -50,24 +83,24 @@ specify_lavaan_bi_model <- function(timepoints,
   resid_covar <- lcsm:::specify_resid_covar(timepoints = timepoints, variable_x = var_x, variable_y = var_y)
   
   # Specify covariances for bivariate latent change score model ----
-  lavaan_bi_growth <- ""
+  lavaan_bi_change <- ""
   
   # Specify covariances between intercepts
-  lavaan_bi_growth <- lcsm:::specify_int_covar(var_x, var_y)
+  lavaan_bi_change <- lcsm:::specify_int_covar(var_x, var_y)
  
-  # Specify covariances between constant growth factors
-  if (model_x$alpha == TRUE){
-    lavaan_bi_growth <- paste(lavaan_bi_growth, lcsm:::specify_int_growth_covar(change_letter = change_letter_x, variable = var_y))
+  # Specify covariances between constant change factors
+  if (model_x$alpha_constant == TRUE){
+    lavaan_bi_change <- paste(lavaan_bi_change, lcsm:::specify_int_change_covar(change_letter = change_letter_x, variable = var_y))
   }
   
-  # Specify covariances between constant growth factors
-  if (model_y$alpha == TRUE){
-    lavaan_bi_growth <- paste(lavaan_bi_growth, lcsm:::specify_int_growth_covar(change_letter = change_letter_y, variable = var_x))
+  # Specify covariances between constant change factors
+  if (model_y$alpha_constant == TRUE){
+    lavaan_bi_change <- paste(lavaan_bi_change, lcsm:::specify_int_change_covar(change_letter = change_letter_y, variable = var_x))
   }
   
-  # Specify covariances between constant growth factors
-  if (model_x$alpha == TRUE & model_y$alpha == TRUE){
-    lavaan_bi_growth <- paste(lavaan_bi_growth, lcsm:::specify_growth_covar(change_letter_x = change_letter_x, change_letter_y = change_letter_y))
+  # Specify covariances between constant change factors
+  if (model_x$alpha_constant == TRUE & model_y$alpha_constant == TRUE){
+    lavaan_bi_change <- paste(lavaan_bi_change, lcsm:::specify_bi_change_covar(change_letter_x = change_letter_x, change_letter_y = change_letter_y))
   }
   
   # Define empty str object 
@@ -96,7 +129,7 @@ specify_lavaan_bi_model <- function(timepoints,
   }
   
   # Combine univariate and bivariate models
-  lavaan_bi_model <- paste(model_x_uni_lavaan, model_y_uni_lavaan, resid_covar, lavaan_bi_growth, lavaan_bi_coupling, sep = "")
+  lavaan_bi_model <- paste(model_x_uni_lavaan, model_y_uni_lavaan, resid_covar, lavaan_bi_change, lavaan_bi_coupling, sep = "")
   
   lavaan_bi_model
 }
