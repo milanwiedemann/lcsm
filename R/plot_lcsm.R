5af8b7754a7971ebba275a150c5b575464e10bfa
Milan Wiedemann <milan.wiedemann@gmail.com>
2018-09-22 13:35:18 +0100

    fix to plot funcitn

--- R/plot_lcsm.R
+++ R/plot_lcsm.R
@@ -1,22 +1,23 @@
 #' Plot latent change score model path diagram
 #'
-#' @param object 
-#' @param layout 
-#' @param what See \code{semPlot}
-#' @param whatLabels 
-#' @param edge.width 
-#' @param node.width 
-#' @param border.width 
-#' @param fixedStyle 
-#' @param freeStyle 
-#' @param residuals 
-#' @param label.scale 
-#' @param sizeMan 
-#' @param sizeLat 
-#' @param intercepts 
-#' @param fade 
-#' @param nCharNodes 
-#' @param nCharEdges 
+#' @param object lavaan object.
+#' @param layout Matrix specifying location of manifest and latent variables. 
+#' @param what See \code{semPlot}.
+#' @param whatLabels see \code{semPlot}.
+#' @param edge.width see \code{semPlot}.
+#' @param node.width  see \code{semPlot}.
+#' @param border.width  see \code{semPlot}.
+#' @param fixedStyle  see \code{semPlot}.
+#' @param freeStyle  see \code{semPlot}.
+#' @param residuals  see \code{semPlot}.
+#' @param label.scale  see \code{semPlot}.
+#' @param sizeMan  see \code{semPlot}.
+#' @param sizeLat  see \code{semPlot}.
+#' @param intercepts  see \code{semPlot}.
+#' @param fade  see \code{semPlot}.
+#' @param nCharNodes  see \code{semPlot}.
+#' @param nCharEdges  see \code{semPlot}.
+#' @param ... Other functions passed on to \code{semPlot}.  
 #'
 #' @return Plot
 #' @export 
@@ -29,37 +30,40 @@ plot_lcsm <- function(object,
                       edge.width = 1,
                       node.width = 1,
                       border.width = 1,
-                      fixedStyle=1,
-                      freeStyle=1,
+                      fixedStyle = 1,
+                      freeStyle = 1,
                       residuals = FALSE,
-                      label.scale=FALSE,
-                      sizeMan = 5,
+                      label.scale = FALSE,
+                      sizeMan = 3,
                       sizeLat = 5,
                       intercepts = FALSE,
                       fade = FALSE,
                       nCharNodes = 0,
-                      nCharEdges = 0) {
+                      nCharEdges = 0,
+                      edge.label.cex = 0.5,
+                      ...) {
   
-  
-  graph <- semPlot::semPaths(fit_lavaan_uni3,
-           what = what,
+  graph <- semPlot::semPaths(
+           object = object,
            layout = layout,
+           what = what,
            whatLabels = whatLabels,
            edge.width = edge.width,
            node.width = node.width,
            border.width = border.width,
-           fixedStyle=fixedStyle,
-           freeStyle=freeStyle,
+           fixedStyle = fixedStyle,
+           freeStyle = freeStyle,
            residuals = residuals,
-           label.scale=label.scale,
+           label.scale = label.scale,
            sizeMan = sizeMan,
            sizeLat = sizeLat,
            intercepts = intercepts,
            fade = fade,
            nCharNodes = 0,
-           nCharEdges = 0)
+           nCharEdges = 0,
+           ...)
   
-  graph$graphAttributes$Edges$curve <- ifelse(graph$Edgelist$bidir, 1.5, 0)
+  graph$graphAttributes$Edges$curve <- ifelse(graph$Edgelist$bidir, 1, 0)
   
   plot(graph)
 }
