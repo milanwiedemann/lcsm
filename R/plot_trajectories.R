afe56e7125c398a3f874e02ea5e31e7dedfec0be
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-03-14 12:36:01 +0100

    fix plot trajectories id group error, thanks graham!

--- R/plot_trajectories.R
+++ R/plot_trajectories.R
@@ -45,9 +45,9 @@ plot_trajectories <- function(data, id_var, var_list, line_colour = "blue", poin
     ggplot2::theme(text = ggplot2::element_text(size = 12))
   
   if (connect_missing == TRUE) {
-    plot <- plot + ggplot2::geom_line(data = data_plot[!is.na(data_plot$value), ], ggplot2::aes(group = id), colour = line_colour, alpha = line_alpha)
+    plot <- plot + ggplot2::geom_line(data = data_plot[!is.na(data_plot$value), ], ggplot2::aes(group = id_var), colour = line_colour, alpha = line_alpha)
     } else if (connect_missing == FALSE) {
-      plot <- plot + ggplot2::geom_line(data = data_plot, ggplot2::aes(group = id), colour = line_colour, alpha = line_alpha)
+      plot <- plot + ggplot2::geom_line(data = data_plot, ggplot2::aes(group = id_var), colour = line_colour, alpha = line_alpha)
       }
   
   if (title_n == TRUE){
