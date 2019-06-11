1c21e708f0915506da71c5b62fceb5354267f68c
Milan Wiedemann <milan.wiedemann@gmail.com>
2019-06-11 16:12:26 +0100

    add stringr:: and change names of funcitons

--- R/plot_trajectories.R
+++ R/plot_trajectories.R
@@ -1,7 +1,5 @@
-
 #' Plot individual trajectories
-
-#'
+#' @description TODO: Describe function
 #' @param data Dataset in wide format.
 #' @param id_var String, specifying id variable.
 #' @param var_list Vector, specifying variable names to be plotted in sequential order.
@@ -9,13 +7,13 @@
 #' @param point_colour String, specifying, colour of points.
 #' @param line_alpha Numeric, specifying alpha of lines.
 #' @param point_alpha Numeric, specifying alpha of points.
-#' @param smooth Logical, add line of average using method = \code{smooth_method}.
-#' @param smooth_method String, specifying method to be used for calculating average line, see  \code{ggplot2}.
+#' @param smooth Logical, add moothed conditional means using \link[ggplot2]{geom_smooth}.
+#' @param smooth_method String, specifying method to be used for calculating average line, see \link[ggplot2]{geom_smooth}.
 #' @param smooth_se Locical, specifying whether to add standard error of average line or not.
 #' @param xlab String for x axis label.
 #' @param ylab String for y axis label.
 #' @param scale_x_num Logical, if \code{TRUE} print sequential numbers starting from 1 as x axis labels, if \code{FALSE} use variable names.
-#' @param scale_x_num_start Numeric, if \code{scale_x_num == TRUE} this is the starting value of the x axis.
+#' @param scale_x_num_start Numeric, if \code{scale_x_num = TRUE} this is the starting value of the x axis.
 #' @param random_sample_frac The fraction of rows to select (from wide dataset), default is set to 1 (100 percent) of the sample.
 #' @param connect_missing Logical, speciying whether to connect points by \code{id_var} across missing values.
 #' @param title_n Logical, speciying whether to print title with number and percentage of cases used for the plot.
@@ -41,24 +39,23 @@ plot_trajectories <- function(data, id_var, var_list, line_colour = "blue", poin
   
   if (connect_missing == TRUE) {
     plot <- plot + ggplot2::geom_line(data = data_plot[!is.na(data_plot$value), ], ggplot2::aes(group = !!rlang::sym(id_var)), colour = line_colour, alpha = line_alpha)
-    } else if (connect_missing == FALSE) {
-      plot <- plot + ggplot2::geom_line(data = data_plot, ggplot2::aes(group = !!rlang::sym(id_var)), colour = line_colour, alpha = line_alpha)
-      }
+  } else if (connect_missing == FALSE) {
+    plot <- plot + ggplot2::geom_line(data = data_plot, ggplot2::aes(group = !!rlang::sym(id_var)), colour = line_colour, alpha = line_alpha)
+  }
   
   if (title_n == TRUE){
-    plot <- plot + ggplot2::ggtitle(paste("N = ", nrow(data), " (", round(random_sample_frac * 100, 2), "% of the sample)", sep = ""))
+    plot <- plot + ggplot2::ggtitle(paste0("N = ", nrow(data), " (", round(random_sample_frac * 100, 2), "% of the sample)"))
   }
   
   if (scale_x_num == FALSE) {
-  plot_x_scale <- plot + ggplot2::scale_x_discrete(labels = var_list)
+    plot_x_scale <- plot + ggplot2::scale_x_discrete(labels = var_list)
   } else {
-  plot_x_scale <- plot + ggplot2::scale_x_discrete(labels = scale_x_num_start:(length(var_list) + scale_x_num_start - 1))
+    plot_x_scale <- plot + ggplot2::scale_x_discrete(labels = scale_x_num_start:(length(var_list) + scale_x_num_start - 1))
   }
-      
+  
   if (smooth == TRUE) {
-  plot_x_scale + ggplot2::geom_smooth(ggplot2::aes(group = 1), size = 1, method = smooth_method, se = smooth_se)
+    plot_x_scale + ggplot2::geom_smooth(ggplot2::aes(group = 1), size = 1, method = smooth_method, se = smooth_se)
   } else {
-  plot_x_scale
+    plot_x_scale
   }
 }
-
