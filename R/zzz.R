23e77788660cc550da333e8c43caea1ea31fa434
Milan Wiedemann <milan.wiedemann@gmail.com>
2021-07-11 09:20:28 +0300

    add cli startup message

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+  
+  version <- read.dcf(file = system.file("DESCRIPTION",
+                                         package = pkgname),
+                      fields = "Version")
+  
+  cli::cli_h1(paste0("This is ", pkgname, " ", version))
+  cli::cli_alert_info(paste0("Please report any issues or ideas at:"))
+  cli::cli_alert_info("https://github.com/milanwiedemann/lcsm/issues")
+}
