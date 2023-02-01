e0224d6bad92d471842d7640803afb55eb11dcd0
milanwiedemann <milan.wiedemann@gmail.com>
2023-02-01 11:08:24 +0200

    Add packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,9 @@
                                          package = pkgname),
                       fields = "Version")
   
-  cli::cli_h1(paste0("This is ", pkgname, " ", version))
-  cli::cli_alert_info(paste0("Please report any issues or ideas at:"))
-  cli::cli_alert_info("https://github.com/milanwiedemann/lcsm/issues/")
+  packageStartupMessage(
+    cli::cli_h1(paste0("This is ", pkgname, " ", version)),
+    cli::cli_alert_info(paste0("Please report any issues or ideas at:")),
+    cli::cli_alert_info("https://github.com/milanwiedemann/lcsm/issues/")
+  )
 }
